5648ffeb57ee7f363bd7ade59c9216859d73eda7
MaNaSDeo <102231033+MaNaSDeo@users.noreply.github.com>
2023-11-08 11:58:46 +0530

    Hero Section Updated

--- src/components/HeroImage/index.jsx
+++ src/components/HeroImage/index.jsx
@@ -4,7 +4,14 @@ import './HeroImage.css'
 function index() {
   return (
     <div className="heroComponent">
-      <img src="heroImage.png" alt="" />
+      <div className='hero-text'>
+        <p>100 Thousand Songs, ad-free</p>
+        <p>Over shousand podcast episodes</p>
+      </div>
+      <div className='hero-img'>
+        <img src="vibrating-headphone 1.png" alt="Hero Headphone Image" height="212px" width="212px" />
+      </div>
+
     </div>
   )
 }
